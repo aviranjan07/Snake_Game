960a06883264955b7880ad3f6d184d7ad95e36ac
aviranjan07 <ad3447578@gmail.com>
2022-03-17 08:22:58 +0530

    updating the snake

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let direction = {x:0, y:0};
+let inputDir = {x:0, y:0};
 const foodSound = new Audio('food.mp3');
 const gameOverSound = new Audio('gameover.mp3');
 const moveSound = new Audio('move.mp3');
 const musicSound = new Audio('music.mp3');
 
 let speed = 2;
+let score = 0;
 let lastPrintTime = 0;
 let snakeArr = [
     {x: 14, y: 15}
@@ -25,6 +26,15 @@ function main(ctime) {
 
 function gameEngine() {
     // updating the snake array and food 
+    if(isCollide(snakeArr)) {
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = {x: 0, y: 0};
+        alert("Game Over. press any key to play again!");
+        snakeArr = [{x: 14, y: 15}];
+        musicSound.play();
+        score = 0;
+    }
 
     // display the snake and food
     board.innerHTML = "";
