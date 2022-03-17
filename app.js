5a379b46a71c26eca072c8f450717fddc0ccfa9e
aviranjan07 <ad3447578@gmail.com>
2022-03-17 09:10:45 +0530

    add conditiion

--- app.js
+++ app.js
@@ -24,8 +24,17 @@ function main(ctime) {
     gameEngine();
 }
 
-function isCollide(sarr) {
-    return false;
+function isCollide(snake) {
+    // if you bupm into yourself
+    for(let i = 1; i < snakeArr.length; i++) {
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+            return true;
+        }
+    }
+    // if you bump into the wall
+    if(snake[0].x >= 18 || snake[0].x <= 0 && snake[0].y >= 18 || snake[0].y <= 0) {
+        return true;
+    }
 }
 function gameEngine() {
     // updating the snake array and food 
